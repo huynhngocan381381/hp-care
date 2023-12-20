1c1eed076d2cd5e3dcddff486ce5003e4a44018f
Huynh Ngoc An <huynhngocan381381@gmail.com>
2023-12-21 01:45:55 +0700

    implement ecommerce functionality

--- src/app/_components/Hero/index.tsx
+++ src/app/_components/Hero/index.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
-
 import { Page } from '../../../payload/payload-types'
+import { CustomHero } from '../../_heros/CustomHero'
 import { HighImpactHero } from '../../_heros/HighImpact'
 import { LowImpactHero } from '../../_heros/LowImpact'
 import { MediumImpactHero } from '../../_heros/MediumImpact'
@@ -9,6 +8,7 @@ const heroes = {
   highImpact: HighImpactHero,
   mediumImpact: MediumImpactHero,
   lowImpact: LowImpactHero,
+  customHero: CustomHero,
 }
 
 export const Hero: React.FC<Page['hero']> = props => {
